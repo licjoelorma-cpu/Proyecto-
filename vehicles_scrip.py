69b54b4cf2e6531015879b8fb8030322a7ad371e
licjoelorma-cpu <lic.joelorma@gmail.com>
2025-09-19 07:29:41 -0600

    Botones

--- vehicles_scrip.py
+++ vehicles_scrip.py
@@ -1,1 +1,37 @@
-print("Hola mundo")
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+     
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+     
+if hist_button: # al hacer clic en el botón
+     # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+     
+
+build_histogram = st.checkbox("Construir un histograma")
+
+if build_histogram:
+    st.write("Construir un histograma para la columna odometro")
+
+
+
+# Botón para gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    # mostrar el gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
